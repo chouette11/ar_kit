5adfa1795f58000c97e703d98e8d2355d9f0f7f9
kei1115 <keifukuda1115@gmail.com>
2021-12-18 20:36:00 +0900

    Change connection key

--- lib/azblob.dart
+++ lib/azblob.dart
@@ -9,7 +9,7 @@ Future uploadImageToAzure(BuildContext context, Uint8List content) async {
     var now = DateTime.now();
     String fileName = "test${now.toString()}";
     // read file as Uint8List
-    var storage = AzureStorage.parse('DefaultEndpointsProtocol=https;AccountName=stortestkeiichiro;AccountKey=X/YAuUAAWwQz8YST5lLj9Tp/noTZAGBAACQaJy/CPTGrefLvFJ0/Xji0Yw4JdpB9z5eEu1FPYMSP93/5sfz5wg==;EndpointSuffix=core.windows.net');
+    var storage = AzureStorage.parse('DefaultEndpointsProtocol=https;AccountName=storageaccountankir99ce;AccountKey=LYbm4AXd2NwOFSeq0xuoxQGo90QncETsh3XuhgbWcR/jHT9sXkx+hM3yLl2jdQSQzDfhXCCkZKHIcSi9APDkcQ==;EndpointSuffix=core.windows.net');
     String container = "test";
     // get the mine type of the file
     await storage.putBlob(
